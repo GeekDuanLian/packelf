500f8248f00861eff9011dddd476a61b5300fbbe
GeekDuanLian <dl@pbstu.com>
2025-12-24 17:09:36 +0800

    fix include

--- debian/nginx.sh
+++ debian/nginx.sh
@@ -6,7 +6,7 @@ bin=(/usr/sbin/nginx)
 etc=(/etc/nginx/mime.types)
 
 # etc
-install_dest /etc/nginx/nginx.conf <<'EOF'
+install_dest /etc/nginx/global.conf <<'EOF'
 user nginx;
 worker_processes auto;
 worker_cpu_affinity auto;
@@ -16,49 +16,58 @@ error_log /var/log/nginx/error.log warn;
 events {
     worker_connections 1024;
 }
+EOF
+install_dest /etc/nginx/http.conf <<'EOF'
+server_tokens off;
+
+log_format main '$remote_addr - $remote_user [$time_local] "$request" '
+                '$status $body_bytes_sent "$http_referer" '
+                '"$http_user_agent" "$http_x_forwarded_for"';
+access_log /var/log/nginx/access.log main;
+
+include      mime.types;
+default_type application/octet-stream;
+
+sendfile on;
+tcp_nopush on;
+tcp_nodelay on;
+keepalive_timeout 65;
+types_hash_max_size 2048;
+
+gzip on;
+gzip_vary on;
+gzip_proxied any;
+gzip_comp_level 6;
+gzip_min_length 1024;
+gzip_types  text/css
+            text/javascript
+            text/xml
+            text/plain
+            application/javascript
+            application/x-javascript
+            application/json
+            application/xml
+            application/rss+xml
+            application/vnd.ms-fontobject
+            font/ttf
+            font/opentype
+            image/svg+xml;
+
+ssl_protocols TLSv1.2 TLSv1.3;
+ssl_ciphers ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384;
+ssl_prefer_server_ciphers off;
+ssl_session_cache shared:SSL:10m;
+ssl_session_timeout 1d;
+ssl_session_tickets off;
+EOF
+install_dest /etc/nginx/nginx.conf <<'EOF'
+include ${dest:?}/etc/nginx/global.conf;
 
 http {
-    server_tokens off;
+    include ${dest:?}/etc/nginx/http.conf;
+    server {
 
-    log_format main '$remote_addr - $remote_user [$time_local] "$request" '
-                    '$status $body_bytes_sent "$http_referer" '
-                    '"$http_user_agent" "$http_x_forwarded_for"';
-    access_log /var/log/nginx/access.log main;
-
-    include      mime.types;
-    default_type application/octet-stream;
-
-    sendfile on;
-    tcp_nopush on;
-    tcp_nodelay on;
-    keepalive_timeout 65;
-    types_hash_max_size 2048;
-
-    gzip on;
-    gzip_vary on;
-    gzip_proxied any;
-    gzip_comp_level 6;
-    gzip_min_length 1024;
-    gzip_types  text/css
-                text/javascript
-                text/xml
-                text/plain
-                application/javascript
-                application/x-javascript
-                application/json
-                application/xml
-                application/rss+xml
-                application/vnd.ms-fontobject
-                font/ttf
-                font/opentype
-                image/svg+xml;
-
-    ssl_protocols TLSv1.2 TLSv1.3;
-    ssl_ciphers ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384;
-    ssl_prefer_server_ciphers off;
-    ssl_session_cache shared:SSL:10m;
-    ssl_session_timeout 1d;
-    ssl_session_tickets off;
+    }
 }
 EOF
 
@@ -120,6 +129,7 @@ id -u "${u}" &>/dev/null || useradd -r -g "${u}" -Md /var/empty/"${u}" -s /usr/s
 # dir
 mkdir -p /etc/nginx
 mkdir -pm700 /var/log/nginx /var/empty/nginx
+install -dm700 -g nginx -o nginx /var/lib/nginx
 
 # etc
 ln -vsf ${dest:?}/etc/nginx/mime.types /etc/nginx/
