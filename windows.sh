a7cee6f0074da43c897b24feb4f62f9fcd8c3906
GeekDuanLian <dl@pbstu.com>
2026-01-04 12:34:09 +0800

    windows

---
+++ windows.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+set -eo pipefail
+echoerr () { echo "${@}" >&2; }; die () { local r="${?}"; echoerr "${@}"; exit "${r}"; }
+trap 'echoerr -e "${0}: \e[0;91mExit with Error Code ${?} at Line ${LINENO}\e[0m"' ERR
+shopt -s extglob globstar nocaseglob dotglob failglob
+
+# var
+pkgver=(2025.89 5420b0c6de08c2e796abe9d0819ce322e244a0d9670678dc750aa07da8426a782b7f8685fa65c8fe053fc5ae0118cc5f31fe7b60d817e6c57000a189f2c97176)
+
+# cygwin
+packages='gcc-core,make,zlib-devel,libcrypt-devel'
+# curl -fsSL -o 'setup.exe' 'https://cygwin.org/setup-x86_64.exe'
+# ./setup.exe --no-admin --quiet-mode --no-shortcuts --no-startmenu --no-desktop --root 'D:\cygwin64' --only-site --site 'http://mirrors.kernel.org/sourceware/cygwin/' --packages "${packages}"
+curl -fsSL -o 'setup_xp.exe' 'http://ctm.crouchingtigerhiddenfruitbat.org/pub/cygwin/setup/snapshots/setup-x86_64-2.874.exe'
+./setup_xp.exe --no-admin --quiet-mode --no-shortcuts --no-startmenu --no-desktop --root 'D:\cygwin64_xp' --no-verify --only-site --site 'http://ctm.crouchingtigerhiddenfruitbat.org/pub/cygwin/circa/64bit/2016/08/30/104235' --packages "${packages}"
+
+# src
+cd "$( mktemp -d )"
+curl -fsSL "https://matt.ucc.asn.au/dropbear/releases/dropbear-${pkgver}.tar.bz2" |
+    tee >(tar -xj --strip 1) | sha512sum -c <(echo "${pkgver[1]} -")
+
+# cfg
+# https://github.com/mkj/dropbear/blob/master/src/default_options.h
+cat >localoptions.h <<'EOF'
+// hide version
+#define IDENT_VERSION_PART ""
+
+// disable pid file
+#define DROPBEAR_PIDFILE "/dev/null"
+
+// disable sftp-server
+#define DROPBEAR_SFTPSERVER 0
+
+// verbose if needed
+#define DEBUG_TRACE 5
+
+// not need motd
+#define DO_MOTD 0
+
+// no agent forwarding
+#define DROPBEAR_SVR_AGENTFWD 0
+
+// not use inetd
+#define INETD_MODE 0
+
+// only aes128
+#define DROPBEAR_AES128 1
+#define DROPBEAR_AES256 0
+#define DROPBEAR_3DES 0
+#define DROPBEAR_CHACHA20POLY1305 0
+
+// only sha256
+#define DROPBEAR_SHA1_HMAC 0
+#define DROPBEAR_SHA2_256_HMAC 1
+#define DROPBEAR_SHA2_512_HMAC 0
+#define DROPBEAR_SHA1_96_HMAC 0
+
+// only ed25519
+#define DROPBEAR_RSA 0
+#define DROPBEAR_RSA_SHA1 0
+#define DROPBEAR_ECDSA 0
+#define DROPBEAR_ED25519 1
+#define DROPBEAR_SK_KEYS 0
+
+// only curve25519
+#define DROPBEAR_DH_GROUP14_SHA1 0
+#define DROPBEAR_DH_GROUP14_SHA256 0
+#define DROPBEAR_DH_GROUP16 0
+#define DROPBEAR_CURVE25519 1
+#define DROPBEAR_SNTRUP761 0
+#define DROPBEAR_MLKEM768 1
+#define DROPBEAR_ECDH 1
+#define DROPBEAR_DH_GROUP1 0
+EOF
+
+# build
+'D:\cygwin64_xp\bin\bash.exe' --noprofile --norc -euo pipefail <<'EOF'
+export PATH='/bin'
+./configure CFLAGS="-fno-stack-protector" \
+    --disable-lastlog \
+    --disable-utmp --disable-utmpx \
+    --disable-wtmp --disable-wtmpx \
+    --disable-pututline --disable-pututxline
+make strip PROGRAMS=dropbear
+EOF
+
+# done
+true
